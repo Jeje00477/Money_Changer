9eb92bac0a659be4fc86faeef44c44a10647c2f0
Jeje00477 <johanesekaristo@gmail.com>
2025-05-21 09:37:01 +0700

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -211,6 +211,7 @@ public class Login extends javax.swing.JFrame {
             rg.pack();
             rg.setLocationRelativeTo(null);
             rg.setDefaultCloseOperation(Register.EXIT_ON_CLOSE);
+            dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
